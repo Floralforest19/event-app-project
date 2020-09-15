375760efe20e9ad5e08700514a01f1f5ebcb5838
Mikaela Fryklund <mikfry@Mikaelas-MacBook.local>
2020-09-15 12:39:03 +0200

    refactor inputValue

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 
 const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/"
 const LOGIN_URL = `${ROOT_URL}auth/api-token-auth/`
 
 function App() {
-  const emailInput = useRef(null)
-  const passwordInput = useRef(null)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  
 
   function login(){
+  
     fetch(LOGIN_URL)
   }
   return (
@@ -16,10 +18,19 @@ function App() {
      <div>
        <div>
          <label htmlFor="email">Email</label>
-         <input ref={emailInput} name="email" placeholder="John.doe@company.com"/>
+         <input 
+         name="email"
+         type="email"
+         value="{email}"
+         onChange={(event) => setEmail(event.currentTarget.value)} 
+         placeholder="John.doe@company.com"/>
        </div>
        <label htmlFor="password">Password</label>
-       <input ref={passwordInput} name="password" type="password"/>
+       <input  
+       name="password" 
+       type="password"
+       value={password}
+       onChange={(event) => setPassword(event.currentTarget.value)}/>
      </div>
      <button onClick={login}>Login</button>
     </div>
