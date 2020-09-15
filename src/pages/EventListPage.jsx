ac31c5b4af58ae136f7a0b603796375bd9ed8436
Mikaela Fryklund <mikfry@Mikaelas-MacBook.local>
2020-09-15 15:07:12 +0200

    create fetchEventList

--- src/pages/EventListPage.jsx
+++ src/pages/EventListPage.jsx
@@ -4,8 +4,20 @@ import { useHistory } from 'react-router-dom'
 const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/"
 const LOGIN_URL = `${ROOT_URL}auth/api-token-auth/`
 
-export default function LoginForm() {
-    
+export default function EventListPage() {
+    const {token} = useContext(UserContext)
+     useEffect(() => {
+         fetchEventList()
+     }, [])
+
+     function fetchEventList() {
+        fetch(EVENT_LIST_URL, {
+            headers: {
+                "Content-Type":"application/json",
+                "Authorzation": `Bearer ${token}`
+            }
+        })
+     }
     return (
         <div>
             <h1>Event List Page</h1>
