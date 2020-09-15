fbd43148b2256e245e273ed6549cb96857cb6f44
Mikaela Fryklund <mikfry@Mikaelas-MacBook.local>
2020-09-15 15:30:36 +0200

    fixt loginform

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,37 +1,31 @@
 import React, { useState } from 'react'
-import { useHistory } from 'react-router-dom'
 
 const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/"
 const LOGIN_URL = `${ROOT_URL}auth/api-token-auth/`
 
 export default function LoginForm() {
-    const history = useHistory()
-
-    const [email, setEmail] = usetState("test.user@willandskill.se")
+    const [email, setEmail] = useState("test.user@willandskill.se")
     const [password, setPassword] = useState("js-lesson-10")
     const [token, setToken] = useState(null)
-    
-
 
     function login() {
         const payload = {
             email: email,
             password: password
         }
-        fetch(LOGIN_URL), {
+        
+        fetch(LOGIN_URL, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(payload)
         })
-        .then(res => res.json())
-        .then(data => {
-            setToken(data.token)
-            history.push("/event-list")
-        })
-
-}
+            .then(res => res.json())
+            .then(data => {
+                setToken(data.token)
+            })
+    }
     return (
         <div>
             <div>
@@ -39,19 +33,21 @@ export default function LoginForm() {
                 <input
                     name="email"
                     type="email"
-                    value="{email}"
+                    value={email}
                     onChange={(event) => setEmail(event.currentTarget.value)}
-                    placeholder="John.doe@company.com" />
+                    placeholder="john.doe@company.com"
+                />
             </div>
-            <label htmlFor="password">Password</label>
-            <input
-                name="password"
-                type="password"
-                value={password}
-                onChange={(event) => setPassword(event.currentTarget.value)} />
+            <div>
+                <label htmlFor="password">Password</label>
+                <input
+                    name="password"
+                    type="password"
+                    value={password}
+                    onChange={(event) => setPassword(event.currentTarget.value)}
+                />
+            </div>
+            <button onClick={login}>Login</button>
         </div>
-        <button onClick={login}>Login</button>
-    </div >
-        </div >
     )
 }
